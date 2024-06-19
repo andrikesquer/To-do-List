46d330ecd0290371e413344ca7baa7a6fa156ef5
andrikesquer <andrikesquer@gmail.com>
2024-06-18 20:08:38 -0600

    Creacion de una página web que contiene una to-do list. Fue realizada utilizando un formulario de tipo text y listas, dando estilo a la página mediante css y la interactividad necesaria para las funciones mediante javascript.

---
+++ scriptt/script.js
@@ -0,0 +1,41 @@
+let escribir = document.getElementById("escribir");
+
+let lista = document.getElementById("lista");
+
+function agregar(){
+    if(escribir.value === ''){
+        alert ("Debes agregar tareas")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = escribir.value;
+        lista.appendChild(li);
+        
+        let eliminar = document.createElement("eliminar");
+        eliminar.innerHTML = "\u00d7";
+        li.appendChild(eliminar);
+    }
+    escribir.value = "";
+    guardar();
+}
+
+lista.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("completada");
+        guardar();
+    }
+    else if(e.target.tagName === "ELIMINAR"){
+        e.target.parentElement.remove();
+        guardar();
+    }
+}, false);
+
+function guardar(){
+    localStorage.setItem("informacion", lista.innerHTML);
+}
+
+function mostrarTareas(){
+    lista.innerHTML = localStorage.getItem("informacion");
+}
+
+mostrarTareas();
